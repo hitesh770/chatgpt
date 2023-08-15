fed85911e8f9cd22fdf2ac381aad34bdf9c31752
Hitesh Vishal <hitesh.vishal@menlosecurity.com>
2023-08-15 20:27:02 +0530

    Added docker file

--- app/main.py
+++ app/main.py
@@ -10,15 +10,5 @@ app = FastAPI(title='email_microservices')
 app.include_router(router)
 
 
-# @app.on_event("startup")
-# async def startup():
-#     app.state.db = await create_engine(connection)
-
-
-# @app.on_event("shutdown")
-# async def shutdown():
-#     app.state.db.close()
-
-
-if __name__ == '__main__':
-    uvicorn.run(app, host=config.host_url, port=config.host_port)
+# if __name__ == '__main__':
+#     uvicorn.run(app, host=config.host_url, port=config.host_port)
